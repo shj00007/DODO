6fe85513d22010f53e31803fa1b3f3dd000a9358
Jet <Jet@Jets-MacBook-Pro.local>
2016-09-01 18:31:03 +0800

    添加文件上传

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import os
+
+UPLOAD_FILE_PATH = "./tempfiles/"
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        #self.write("Hello askdf, world")
-	self.render("./templates/main.html", title = "Main Page");
+        self.render("./templates/main.html", title = "Main Page");
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     all_client = set()
@@ -23,12 +25,23 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
         print("WebSocket closed")
         EchoWebSocket.all_client.remove(self)
 
+class UploadHandler(tornado.web.RequestHandler):
+    def post(self):
+        uploadFile = self.request.files['uploadFile'][0]
+        filename = uploadFile['filename']
+        fileObj = open(UPLOAD_FILE_PATH + filename, 'wb')
+        fileObj.write(uploadFile['body'])
+        for client in EchoWebSocket.all_client:
+            client.write_message(u"You said: " + message)
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/websocket", EchoWebSocket),
-    ], debug=True)
+        (r'/upload', UploadHandler),
+        (r"/tempfiles/(.*)", tornado.web.StaticFileHandler, {"path": UPLOAD_FILE_PATH}),
+    ], static_path = os.path.join(os.path.dirname(__file__), "static"), debug=True)
 
 if __name__ == "__main__":
     app = make_app()
